c2e037b3901ddd9ad016c4210a545d00d0732972
Jose Caicedo <pcaicedoec@gmail.com>
2022-09-07 10:46:36 -0500

    Commit inicial sesion 04

---
+++ src/fundamentos/sesion04/clases/Empleado.java
@@ -0,0 +1,15 @@
+package fundamentos.sesion04.clases;
+
+import java.time.LocalDate;
+
+public class Empleado 
+{
+   //! atributos
+   int codigo;
+   String cedula;
+   String nombresApellidos;
+   LocalDate fechaNacimiento;
+   String telefonos;
+   char genero; 
+   double salario;  
+}
---
+++ src/fundamentos/sesion04/clases/Monitor.java
@@ -0,0 +1,15 @@
+package fundamentos.sesion04.clases;
+
+public class Monitor 
+{
+   //! variables ---> atributos (caracteristicas)
+   String marca;
+   String modelo;
+   String color;
+   int tamanio;
+   boolean estaEncendido;
+
+   //! funciones ---> metodos (comportamiento)
+
+   
+}
