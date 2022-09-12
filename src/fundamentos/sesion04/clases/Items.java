e5306805ca557a2b67198ba7ff058d006e6cec7c
Renato Rodriguez <renato_rodriguez123@hotmail.com>
2022-09-12 14:20:31 -0500

    Se creó la clase Item.

---
+++ src/fundamentos/sesion04/clases/Items.java
@@ -0,0 +1,18 @@
+package fundamentos.sesion04.clases;
+
+import java.time.LocalDate;
+
+public class Items 
+{
+    //! variables ---> atributos (caracteristicas)
+    
+    private int idItem;
+    private int codigo;
+    private String nombre;
+    private  int precio;
+    private String nombreProv;
+    private LocalDate fechaIngreso;
+    
+    //! funciones ---> metodos (comportamiento)
+
+}
